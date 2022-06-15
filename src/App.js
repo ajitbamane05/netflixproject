9fdf0e2de10174889b1f5b47dcd080d2e3146890
ajitbamane05 <ajitbamane05@gmail.com>
2022-06-15 08:09:14 +0530

    Signup page commented

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import Homepage from './pages/homepage';
 import Signpage from './pages/Signpage';
-import SignUpPage from './pages/SignUpPage';
+// import SignUpPage from './pages/SignUpPage';
 import {Routes,Route} from 'react-router-dom'
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Homepage/>}>  </Route>
           <Route path='/signin' element={<Signpage/>}> </Route>
-          <Route path='/signup' element={ <SignUpPage/>}> </Route>
+          {/* <Route path='/signup' element={ <SignUpPage/>}> </Route> */}
         </Routes>
       
     </>
